efff3d803d3f4064dd2a28cca8b77e9ead9ec637
tuchucheng <tuchucheng@xiaomi.com>
2019-08-12 18:15:41 +0800

    modify proj file

--- mi_config.h
+++ mi_config.h
@@ -2,7 +2,7 @@
 #define __MI_CONFIG_H__
 #include <stdint.h>
 
-#define PRODUCT_ID             0x01CF
+#define PRODUCT_ID             156//0x01CF
 
 #define EVT_MAX_SIZE           16
 #define EVT_QUEUE_SIZE         8
